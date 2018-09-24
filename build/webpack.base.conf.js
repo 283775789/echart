81d190bdfd01711d33a9b717102438ac1bb11428
陈小七 <283775789@qq.com>
2018-09-25 07:43:03 +0800

    '新提交'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: ['babel-polyfill', config.entry]
+    app: [config.entry]
   },
   output: {
     path: config.build.assetsRoot,
