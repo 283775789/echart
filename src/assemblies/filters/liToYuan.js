bc3cf38294396e0f37dcc7f59e8877c76861839c
陈小七 <283775789@qq.com>
2018-09-25 07:29:01 +0800

    '新提交'

--- src/assemblies/filters/liToYuan.js
+++
@@ -1,4 +0,0 @@
-export default function (value) {
-  let r = Math.floor(value / 10) / 100
-  return isNaN(r) ? 0 : r
-}
--- src/assemblies/filters/liToYuanD3.js
+++
@@ -1,4 +0,0 @@
-export default function (value) {
-  let r = value / 1000
-  return isNaN(r) ? 0 : r
-}
